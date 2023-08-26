8b151183b2a1bb58a5bd26416b92785b5fe5eb66
Matias de Andrea <deandreamatias@gmail.com>
2023-08-26 13:30:31 +0200

    feat: create iframe widget

--- lib/ui/widgets/screens/home/home_screen.dart
+++ lib/ui/widgets/screens/home/home_screen.dart
@@ -1,9 +1,9 @@
 import 'package:dog_gromming_website/di/di.dart';
 import 'package:dog_gromming_website/ui/viewmodels/home/home_viewmodel.dart';
 import 'package:dog_gromming_website/ui/widgets/components/error_card.dart';
+import 'package:dog_gromming_website/ui/widgets/components/iframe.dart';
 import 'package:dog_gromming_website/ui/widgets/components/loading_circle.dart';
 import 'package:dog_gromming_website/ui/widgets/screens/root_screen.dart';
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends RootScreen<HomeViewState> {
@@ -18,16 +18,7 @@ class HomeScreen extends RootScreen<HomeViewState> {
       body: SafeArea(
         child: switch (state) {
           Loading _ => const LoadingCircle(),
-          Success _ => state.list.isEmpty
-              ? Center(child: Text("empty_list".tr()))
-              : ListView.builder(
-                  itemCount: state.list.length,
-                  itemBuilder: (context, index) {
-                    return const ListTile(
-                      title: Text(''),
-                    );
-                  },
-                ),
+          Success _ => const Iframe(url: ''),
           Error _ => ErrorCard(error: state.error),
         },
       ),
