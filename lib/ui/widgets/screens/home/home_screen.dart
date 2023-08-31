3c5bfe12a83091f28444e8616bdad539758536bc
Matias de Andrea <deandreamatias@gmail.com>
2023-08-31 23:40:35 +0200

    feat: WIP show info on home

--- lib/ui/widgets/screens/home/home_screen.dart
+++ lib/ui/widgets/screens/home/home_screen.dart
@@ -1,5 +1,11 @@
 import 'package:dog_gromming_website/ui/styles/app_colors.dart';
+import 'package:dog_gromming_website/ui/styles/insets.dart';
 import 'package:dog_gromming_website/ui/widgets/components/assets/svg_image.dart';
+import 'package:dog_gromming_website/ui/widgets/components/box_spacer.dart';
+import 'package:dog_gromming_website/ui/widgets/components/buttons/primary_button.dart';
+import 'package:dog_gromming_website/ui/widgets/components/texts/body_l_text.dart';
+import 'package:dog_gromming_website/ui/widgets/components/texts/body_m_text.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -7,12 +13,43 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const _ResponsiveWelcome();
+    return _ResponsiveWelcome(
+      child: Padding(
+        padding: Insets.a16,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(child: BodyLText('home.slogan'.tr())),
+            BoxSpacer.v16(),
+            Expanded(
+              child: Column(
+                children: [
+                  BodyMText('home.appointment'.tr()),
+                  BoxSpacer.v16(),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      PrimaryButton(
+                        onPressed: () {},
+                        label: 'home.contact_number'.tr(),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            BoxSpacer.v16(),
+          ],
+        ),
+      ),
+    );
   }
 }
 
 class _ResponsiveWelcome extends StatelessWidget {
-  const _ResponsiveWelcome();
+  final Widget child;
+
+  const _ResponsiveWelcome({required this.child});
 
   @override
   Widget build(BuildContext context) {
@@ -20,11 +57,16 @@ class _ResponsiveWelcome extends StatelessWidget {
       builder: (context, constraints) {
         final double screenWidth = constraints.maxWidth;
         final double screenHeight = constraints.maxHeight;
-        // TODO: Implement small landscape layout
 
         final double rectWidth = screenWidth * 0.8;
         final double rectHeight = screenHeight * 0.7;
-        final double circleRadius = rectWidth * 0.25;
+        double circleRadius = screenWidth * 0.2;
+
+        // Verify if the circle radius is off the screen
+        // (behaind the top nav bar)
+        if (constraints.maxHeight < rectHeight + circleRadius * 1.34) {
+          circleRadius = screenHeight * 0.2;
+        }
 
         return Stack(
           children: [
@@ -37,6 +79,12 @@ class _ResponsiveWelcome extends StatelessWidget {
                 decoration: const BoxDecoration(
                   color: AppColors.primary,
                   borderRadius: BorderRadius.vertical(top: Radius.circular(8)),
+                ),
+                child: Column(
+                  children: [
+                    BoxSpacer(verticalSpacing: circleRadius * 0.66),
+                    Expanded(child: Center(child: child)),
+                  ],
                 ),
               ),
             ),
