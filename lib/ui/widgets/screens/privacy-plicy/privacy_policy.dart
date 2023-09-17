9aa838afaa03182c4cfe3286877d193c45a9d3b8
Matias de Andrea <deandreamatias@gmail.com>
2023-09-17 12:59:37 +0200

    feat: add privacy policy

---
+++ lib/ui/widgets/screens/privacy-plicy/privacy_policy.dart
@@ -0,0 +1,61 @@
+import 'package:dog_gromming_website/env/constants.dart';
+import 'package:dog_gromming_website/ui/styles/insets.dart';
+import 'package:dog_gromming_website/ui/widgets/components/box_spacer.dart';
+import 'package:dog_gromming_website/ui/widgets/components/loading_circle.dart';
+import 'package:dog_gromming_website/ui/widgets/components/texts/body_m_text.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
+
+final _modifiedPrivacy = DateTime(2023, 09, 17);
+
+class PrivacyPolicy extends StatelessWidget {
+  const PrivacyPolicy({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('privacy_policy.title'.tr()),
+      ),
+      body: Padding(
+        padding: Insets.a20,
+        child: FutureBuilder(
+          future: rootBundle.loadString(Constants.privacyPolicy),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return SelectionArea(
+                child: SingleChildScrollView(
+                  physics: const BouncingScrollPhysics(),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      HtmlWidget(
+                        snapshot.requireData,
+                        onLoadingBuilder: (context, element, loadingProgress) =>
+                            const LoadingCircle(),
+                      ),
+                      BoxSpacer.v16(),
+                      BodyMText(
+                        'privacy_policy.modify_date'.tr(
+                          args: [
+                            _modifiedPrivacy
+                                .toLocal()
+                                .toString()
+                                .replaceAll('00:00:00.000', ''),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            }
+            return const LoadingCircle();
+          },
+        ),
+      ),
+    );
+  }
+}
